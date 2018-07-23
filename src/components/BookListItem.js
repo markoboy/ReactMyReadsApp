88c03b59350500a45649e9c5ea85338e3412c116
Athanasios Markou <thanasismarko@gmail.com>
2018-07-23 14:37:52 +0100

    feat: Add book-no-cover img for books that have no cover

--- src/components/BookListItem.js
+++ src/components/BookListItem.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 
 class BookListItem extends Component {
 	static propTypes = {
-		book: PropTypes.object.isRequired
+		book: PropTypes.object.isRequired,
+		onBookMove: PropTypes.func.isRequired
 	};
 
 	render() {
@@ -13,7 +14,13 @@ class BookListItem extends Component {
 			<li>
 				<div className="book">
 					<div className="book-top">
-						<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+						<div className={!book.imageLinks ? 'book-cover book-no-cover' : 'book-cover'}
+							style={
+								book.imageLinks ?
+									{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` } :
+									{ width: 128, height: 193 }
+							}
+						></div>
 						<div className="book-shelf-changer">
 							<select value={book.shelf ? book.shelf : 'none'} onChange={(e) => onBookMove(book, e.target.value)}>
 								<option value="move" disabled>Move to...</option>
